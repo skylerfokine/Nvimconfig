cf545247e54706a1e4e2e47da37d7fe236dddafb
Skyler Fokine <skylerfokine@gmail.com>
2025-01-20 01:06:30 -0500

    configured neogen to use doxygen for annotation convention

--- init.lua
+++ init.lua
@@ -11,17 +11,23 @@ vim.keymap.set("n", "<leader>sh", ":split<CR>") -- Split Horizontally
 vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- Toggle Minimise
 
 --Execution bindings
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")--executes the source command for curent file
-vim.keymap.set("n", "<space>x", ":.lua<CR>")--executes the current line as lua code
-vim.keymap.set("v", "<space>x", ":lua<CR>")--executes the selected visual block
-vim.keymap.set("n", "<space>et","<cmd>tab split<CR>") -- full screen the current window
-vim.keymap.set("n", "<space>te","<cmd>tabc<CR>") -- close the current window
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") --executes the source command for curent file
+vim.keymap.set("n", "<space>x", ":.lua<CR>") --executes the current line as lua code
+vim.keymap.set("v", "<space>x", ":lua<CR>") --executes the selected visual block
+vim.keymap.set("n", "<space>et", "<cmd>tab split<CR>") -- full screen the current window
+vim.keymap.set("n", "<space>te", "<cmd>tabc<CR>") -- close the current window
+vim.api.nvim_set_keymap("n", "<leader>ng", ":Neogen<CR>", { noremap = true, silent = true }) -- run neogen
 
 -- Directory Navigation
 vim.keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 -- Link acessesing
-vim.api.nvim_set_keymap("n","gx",[[:silent execute '!cmd.exe /C start firefox ' . shellescape(expand('<cfile>'), 1)<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"gx",
+	[[:silent execute '!cmd.exe /C start firefox ' . shellescape(expand('<cfile>'), 1)<CR>]],
+	{ noremap = true, silent = true }
+)
 
 -- Comments
 vim.api.nvim_set_keymap("n", "<C-_>", "gtc", { noremap = false })
@@ -31,9 +37,9 @@ vim.api.nvim_set_keymap("v", "<C-_>", "goc", { noremap = false })
 --  Try it with `yap` in normal mod
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- lua/config/plugins/neogen.lua
+++ lua/config/plugins/neogen.lua
@@ -1,7 +1,27 @@
-return{
-    "danymat/neogen",
-    lazy = false,
-    config = true,
-    -- only stable versions
-    version = "*"
+return {
+	"danymat/neogen",
+	lazy = false,
+	config = function()
+		require("neogen").setup({
+			enabled = true,
+			languages = {
+				cpp = {
+					template = {
+						annotation_convention = "doxygen", -- Use Doxygen for C++
+					},
+				},
+				python = {
+					template = {
+						annotation_convention = "google", -- Example for Python
+					},
+				},
+				lua = {
+					template = {
+						annotation_convention = "ldoc", -- Example for Lua
+					},
+				},
+			},
+		})
+	end,
+	version = "*", -- Use the latest stable version
 }
