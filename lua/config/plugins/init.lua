96f31b12637908975154527d416486eb15066a03
Skyler Fokine <skylerfokine@gmail.com>
2025-02-05 15:45:39 -0500

    adding git icons to tell changes in editor need to add luasage status line part

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,5 +1,6 @@
-return{
+return {
 
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",
+	{ "lewis6991/gitsigns.nvim", lazy = false },
 }
