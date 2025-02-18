2bf8d669d8c104238f805c0b2415c179957c38a2
Skyler Fokine <skylerfokine@gmail.com>
2025-02-18 14:35:31 -0500

    auto reload on write with NvimTree

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,11 +1,12 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-} 
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	auto_reload_on_write = true,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({})
+	end,
+}
