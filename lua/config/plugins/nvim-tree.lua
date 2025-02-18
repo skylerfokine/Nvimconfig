c9c5dd78cfc999a4e9a73bd36f6421f061fda8e3
Skyler Fokine <skylerfokine@gmail.com>
2025-02-18 14:46:36 -0500

    made it so i can see files that are ignored by git

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -2,6 +2,9 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	version = "*",
 	lazy = false,
+	git = {
+		ignore = false,
+	},
 	auto_reload_on_write = true,
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
