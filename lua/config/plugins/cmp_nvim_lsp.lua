1063d6ecc6bdcb3ff7e9508ab5ab544362b6df42
Skyler Fokine <skylerfokine@gmail.com>
2025-02-27 11:23:50 -0500

    added vscode like snippets

--- lua/config/plugins/cmp_nvim_lsp.lua
+++ lua/config/plugins/cmp_nvim_lsp.lua
@@ -5,7 +5,7 @@ return {
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
 
-		require("luasnip/loaders/from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
 		vim.opt.completeopt = "menu,menuone,noselect"
 
